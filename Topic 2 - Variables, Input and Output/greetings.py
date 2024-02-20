09faa71f969c560ae19dfe74587b5e4981f3ce10
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-20 12:48:42 +0300

    Delete Topic 2 - Variables, Input and Output directory

--- Topic 2 - Variables, Input and Output/greetings.py
+++
@@ -1,6 +0,0 @@
-name = input('Please enter your name')
-print('Hello, ' + name)
-
-letters = len(name)
-print(letters)
-print('Your name has ' str(letters) +' letters'
--- Topic 2 - Variables, Input and Output/road_trip.py
+++
@@ -1,4 +0,0 @@
-miles_to_portland = 1425.84
-miles_to_helena = 907.29
-miles_helena_to_portland = miles_to_portland - miles_to_helena
-print(miles_helena_to_portland)
