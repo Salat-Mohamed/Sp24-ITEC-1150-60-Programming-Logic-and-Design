9e56f2e4f99e59837e769412b159398513092e1a
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-20 13:48:05 +0300

    Create quiz_score.py

---
+++ Lecture Code-Variables,If Statements/quiz_score.py
@@ -0,0 +1,6 @@
+quiz_score = float(input('Enter sor for quiz, out of 100: '))
+
+if quiz_score == 100:
+  print( You aced the quiz!')
+  print('Congratulations!)
+print('This is the end of the program')
