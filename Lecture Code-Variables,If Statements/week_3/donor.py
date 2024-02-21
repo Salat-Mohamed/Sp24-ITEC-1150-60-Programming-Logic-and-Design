5173f452885649a6f76d42ea1ac921eb2510e29c
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 10:16:15 +0300

    Update donor.py

--- Lecture Code-Variables,If Statements/week_3/donor.py
+++ Lecture Code-Variables,If Statements/week_3/donor.py
@@ -5,3 +5,7 @@ if weight >= 110 and age >= 16:
   print('Great! you are eligile to be a blood donor.')
 else:
   print('Sorry, you are not eligible')
+  if age < 16:
+    print('You are not old enough')
+  if weight < 110:
+    print('You do not weigh enough')
