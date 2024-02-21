2a90d45e896950aeea5003e4fb935d468d846635
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 12:24:00 +0300

    Update books.py

--- Lecture Code and Notes - Loops and Functions/week_4/books.py
+++ Lecture Code and Notes - Loops and Functions/week_4/books.py
@@ -6,6 +6,9 @@ total = 0
 
 for book in range(number_of_books):
   book_price = float(input('Enter book price in $'))
+  if book_price == 0:
+    print('Yay! Free book!')
   total = total +book_price
+  
 #print('The total price for all books is ' + str(total))
 print(f'The total price for all books is {total}')
