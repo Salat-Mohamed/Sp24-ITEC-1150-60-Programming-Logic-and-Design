c81ed806efec57a5f9263457238377bbea50b88b
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 12:43:37 +0300

    Update credits.py

--- Lecture Code-Variables,If Statements/week_3/credits.py
+++ Lecture Code-Variables,If Statements/week_3/credits.py
@@ -1,6 +1,9 @@
-credits = int(input('How many credits are you taking this semester?')
+number_of_credits = int(input('How many credits are you taking this semester?'))
 
-if number_of _credits >= 12:
+while number_of_credits < 0:
+  print('Error - please enter 0 or a positive number')
+ 
+if number_of_credits >= 12:
   print('You are a full-time student')
 elif number_of_credits >= 6:
   print('You are a half-time student')
