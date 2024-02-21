190c166296cea09c4ddd2df2be577c29f2122f28
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 08:52:38 +0300

    Create parking.py

---
+++ Lecture Code-Variables,If Statements/week_3/parking.py
@@ -0,0 +1,11 @@
+parking_time = float(input('How many hours have you parked? '))
+
+# The max parking time allowed is 2 hours
+
+if parking_time >= 2:
+  print('Warining! You should move your car.')
+
+# pause the video
+# write this code
+# compare > version to the >= version
+# which do you think is better?
