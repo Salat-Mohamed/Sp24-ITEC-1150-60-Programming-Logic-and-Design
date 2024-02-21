795df27b4f842c66a2ab4356faa50473718a31c7
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 09:58:37 +0300

    Create donor.py

---
+++ Lecture Code-Variables,If Statements/week_3/donor.py
@@ -0,0 +1,7 @@
+weight = float(input('please enter your weight in pounds: '))
+age = int(input('please enter your age in years: '))
+
+if weight >= 110 and age >= 16:
+  print('Great! you are eligile to be a blood donor.')
+else:
+  print('Sorry, you are not eligible')
