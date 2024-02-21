1d9a3800c44eac928e686ca9517e4039bb798c7e
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 10:08:34 +0300

    Create classes.py

---
+++ Lecture Code-Variables,If Statements/week_3/classes.py
@@ -0,0 +1,7 @@
+csharp = input('Have you taken the C# programming class? Type "Yes" if so: ')
+java = input('Have you taken the Java programming class? Type "Yes" if so: ')
+
+if csharp == 'Yes' or java == 'Yes':
+  print('You can take ios programming!')
+else:
+  print('You need to take either C# or Java')
