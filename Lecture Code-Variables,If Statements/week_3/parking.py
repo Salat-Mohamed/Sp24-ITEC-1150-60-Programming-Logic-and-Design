58dc76ff419f14df663cb971017570cf83fd18ef
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 09:07:15 +0300

    Update parking.py

--- Lecture Code-Variables,If Statements/week_3/parking.py
+++ Lecture Code-Variables,If Statements/week_3/parking.py
@@ -4,7 +4,15 @@ parking_time = float(input('How many hours have you parked? '))
 
 if parking_time >= 2:
   print('Warining! You should move your car.')
-
+  # can you print how many hours you are over?
+  time_over = parking_time - 2
+  print('You are ' + str(time_over) + ' hours overdue')
+else:
+  print('You are ok for parking, you still have time')
+  # Can you print how many hours are left?
+  time_left = 2 - parking_time
+  print('You have ' + str(time_left) + ' hours left.')
+print('This is the end of the program')
 # pause the video
 # write this code
 # compare > version to the >= version
