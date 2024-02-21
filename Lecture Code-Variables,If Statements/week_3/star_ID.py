d6162ef70cc5e1e1006dea1bcd5f45abc4563dd1
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 09:48:49 +0300

    Create star_ID.py

---
+++ Lecture Code-Variables,If Statements/week_3/star_ID.py
@@ -0,0 +1,8 @@
+star_id = input('Please enter your StarID: ')
+
+if len(star_id) == 8:
+  print('Your starID is the correct length')
+else:
+  print('Your starID is not the right length')
+# your turn - print different messages for too long.
+# message for too short.
