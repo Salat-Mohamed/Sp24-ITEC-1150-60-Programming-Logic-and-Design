796e115f300f1661a19c9fb76021bbfa02ca9f93
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-21 09:20:46 +0300

    Create quiz.py

---
+++ Lecture Code-Variables,If Statements/week_3/quiz.py
@@ -0,0 +1,8 @@
+answer = input('What is the state capital of Wisconsin') # its Madison
+
+if answer.upper() == 'Madison':
+  print('Correct!')
+else:
+  print('The capital of Wisconsin is Madison.')
+# run and test.
+# extra question - can you make the check case-insensitive?
