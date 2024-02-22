70388f1cdebd495cc48e4fc07509de3aa3cefa9e
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-22 12:25:24 +0300

    Create cubes.py

---
+++ Lecture Code and Notes - Loops and Functions/week_5/cubes.py
@@ -0,0 +1,10 @@
+def main():
+  for number in range(10):
+    c = cube(number)
+    print(c)
+def cube(value):
+  cube_value = value * value * value
+  # cube_valu = value**3
+  return cube value
+main()
+  
