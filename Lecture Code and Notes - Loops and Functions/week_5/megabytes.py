e152791609efb79bc1a0c50fd438a8af2cf149f9
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-22 12:21:15 +0300

    Create megabytes.py

---
+++ Lecture Code and Notes - Loops and Functions/week_5/megabytes.py
@@ -0,0 +1,10 @@
+def megabytes_to_bytes(megabytes):
+  bytes = megabytes * 1000000
+  return bytes
+
+def main():
+  b = megabytes_to_bytes(10) 
+  print(b)
+  b = megabytes_to_bytes(64)
+  print(b)
+main()
