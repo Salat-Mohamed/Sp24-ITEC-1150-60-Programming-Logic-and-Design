b6d7f75e7deb23b16b897ba244dc8e2f5569c117
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-22 12:02:28 +0300

    Update functions.py

--- Lecture Code and Notes - Loops and Functions/week_5/functions.py
+++ Lecture Code and Notes - Loops and Functions/week_5/functions.py
@@ -1,1 +1,11 @@
+print('Hello!')
 
+name = input('please enter your name: ')
+
+print('hello, ' + name + '!')
+
+for number in range(5,10):
+  print(number)
+
+for letter in 'hello':
+  print(letter )
