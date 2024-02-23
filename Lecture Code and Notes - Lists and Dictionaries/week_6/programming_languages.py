966bc59b66ed8f54d38ff06c48653ccf48d2137b
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-23 11:29:10 +0300

    Create programming_languages.py

---
+++ Lecture Code and Notes - Lists and Dictionaries/week_6/programming_languages.py
@@ -0,0 +1,12 @@
+languages = ['Python','java','Javascript','C#','Swift']
+
+for language in languages:
+  print(language)
+all_languages = ''.join(languages)
+print(all_languages)
+
+languages.sort()
+print(languages)
+
+languages.reverse()
+print(languages)
