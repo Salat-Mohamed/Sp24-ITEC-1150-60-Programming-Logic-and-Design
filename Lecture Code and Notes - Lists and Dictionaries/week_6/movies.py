2fc7c7f2a42cf78899ba93c6b2f8ef7c39a2a8a2
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-23 11:34:50 +0300

    Update movies.py

--- Lecture Code and Notes - Lists and Dictionaries/week_6/movies.py
+++ Lecture Code and Notes - Lists and Dictionaries/week_6/movies.py
@@ -5,3 +5,10 @@ movies.reverse()
 
 for movie in movies:
   print(movie)
+
+# add a new movie to the end of the list?
+ 
+movies.append('Spiderman')
+print(movies)
+movies.append('Lion King')
+print(movies)
