764d076b4b179af2c311be3a54f21704b8242021
Salat-Mohamed <158267668+Salat-Mohamed@users.noreply.github.com>
2024-02-23 10:40:50 +0300

    Create streets.py

---
+++ Lecture Code and Notes - Lists and Dictionaries/week_6/streets.py
@@ -0,0 +1,20 @@
+streets =['Lake','Hennepin','Lyndale']
+print(streets)
+
+print(streets[0])
+print(streets[1])
+print(streets[2])
+
+streets[0] = 'Chicago'
+print(streets)
+
+# Can you modify element 1 to a different name?
+# Can you modify element 2 to a different name?
+# print the list to verify
+streets[2] = '1st Avenue'
+streets[1] = 'Broadway'
+
+print(streets)
+
+streets[3] = park
+
